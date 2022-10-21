2cb78d0d134a825468f4a6a0405aa90f500a3d2d
ronaferr <ferreiraronaldocarlos@gmail.com>
2022-10-21 16:19:51 -0300

    criação de interface IModel generica

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,9 @@
+interface IModel<T> {
+    create(obj:T):Promise<T>,
+    read():Promise<T[] |null>,
+    readOne(_id:string):Promise<T |null>,
+    update(_id: string, obj:T):Promise<T |null>,
+    delete(_id: string):Promise<T |null>,
+};
+
+export { IModel };
